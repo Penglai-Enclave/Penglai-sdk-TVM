4390844dbee2296e0c4fc4a8599a387c81784527
fengeh <2748250768@qq.com>
2021-02-08 23:50:07 +0800

    rectify the sdk path in the file.mk

--- file.mk
+++ file.mk
@@ -1,6 +1,6 @@
 SDK_FILES = \
-	$(srcdir)/penglai-sdk/enclave-driver/penglai.ko \
-	$(srcdir)/penglai-sdk/demo/host/host \
-	$(srcdir)/penglai-sdk/demo/hello-world/hello-world \
-	$(srcdir)/penglai-sdk/demo/relay-page/relay-page \
-	$(srcdir)/penglai-sdk/demo/mem/mem
+	$(srcdir)/Penglai-sdk-TVM/enclave-driver/penglai.ko \
+	$(srcdir)/Penglai-sdk-TVM/demo/host/host \
+	$(srcdir)/Penglai-sdk-TVM/demo/hello-world/hello-world \
+	$(srcdir)/Penglai-sdk-TVM/demo/relay-page/relay-page \
+	$(srcdir)/Penglai-sdk-TVM/demo/mem/mem
