4a8cd7c04d00418139ca4613215631137b5fa414
luxu <1603155230@qq.com>
2021-03-27 16:39:46 +0800

    add the small demo

---
+++ demo/small-demo/small-demo2/small-demo.c
@@ -0,0 +1,21 @@
+#include "eapp.h"
+#include "print.h"
+
+const char data[2*1024*1024] = {1,};
+
+int hello(unsigned long * args)
+{
+  //eapp_print("hello world!\n");
+}
+
+int EAPP_ENTRY main(){
+  unsigned long * args;
+  EAPP_RESERVE_REG;
+
+  unsigned long boot_end;
+  asm volatile("rdcycle %0" : "=r"(boot_end));
+
+  hello(args);
+
+  EAPP_RETURN(boot_end);
+}
