67b0bdec490317a89877fc3c7e4024fb576a7ef2
fengeh <2748250768@qq.com>
2021-07-02 10:43:57 +0800

    add persistency storage

--- enclave-driver/penglai-enclave-ioctl.c
+++ enclave-driver/penglai-enclave-ioctl.c
@@ -176,7 +176,6 @@ int penglai_enclave_create(struct file *filep, unsigned long args)
     goto destroy_enclave;
   }
 
-  penglai_dprintf("stack size %lx\n", stack_size);
   if(penglai_enclave_eapp_loading(enclave->enclave_mem, elf_ptr, elf_size, 
         &elf_entry, STACK_POINT, stack_size, enclave->type))
   {
--- enclave-driver/penglai-enclave-persistency.h
+++ enclave-driver/penglai-enclave-persistency.h
@@ -2,7 +2,7 @@
 #define _PENGLAI_ENCLAVE_PERSISTENCY_
 #define FS_INIT_SIZE_ORDER (10)  // PERSISTENCY_FILE initialize with 4M size
 #define BSIZE (4096)
-#define PERSISTENCY_FILE_PATH ("/tmp/persistencty_file")
+#define PERSISTENCY_FILE_PATH ("/mnt/persistencty_file")
 int penglai_fs_persistency_init(struct file *filep, unsigned long args);
 int penglai_outer_init(unsigned long shm_id_p);
 int penglai_outer_read(unsigned long sec);
