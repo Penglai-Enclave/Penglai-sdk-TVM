3d7ad8c780b850a0d38bd688688aa92329fbee85
fengeh <2748250768@qq.com>
2022-05-06 01:06:06 +0800

    add the attacker's code
    Signed-off-by: fengeh <2748250768@qq.com>

--- demo/rop-attack/rop-attack.c
+++ demo/rop-attack/rop-attack.c
@@ -1,11 +1,13 @@
 #include "eapp.h"
 #include "print.h"
 #include <stdlib.h>
+#include <string.h>
 
 int buffer_overflow()
 {
   long buf[1];
-  long attack_address = 0;
+  long attack_address = 0x1150;
+  eapp_print("return address %lx\n", *(long *)(buf+2));
   memcpy(buf+2, &attack_address, 8);
   nest_call();
   return 0; 
@@ -13,14 +15,21 @@ int buffer_overflow()
 
 int nest_call()
 {
-    printf("nest call\n");
+    eapp_print("nest call\n");
     return 0;
+}
+
+int attack_code()
+{
+  // while(1) {}
+  eapp_print("goto the attacker's code\n");
+  EAPP_RETURN(0);
 }
 
 
 int hello(unsigned long * args)
 {
-  printf("hello world!\n");
+  eapp_print("hello world!\n");
   buffer_overflow();
   EAPP_RETURN(0);
 }
